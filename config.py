fd1d00905fc5c563b44b23266f742fb0a5b087fe
Iftach Danciger <dentzbar@gmail.com>
2025-05-13 15:40:35 +0300

    modify to .env file all tokens

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
     JIRA_API_TOKEN = 'ATATT3xFfGF0-sLnkANXgjwfk9cPJttksyTXoF6JiN5iwa-TYkmC4F4YggW9GOkyNrdPqH1nRlKtc9zcoj6EZ1zrsKCFukfTfBYIblTHNCRMLrI8DhL734mQntZlIF7zzU5pZ5GdAco6LMKKG-bfpozIDKNBI4nCMleF-dXG0zxlMW7XCsvYWdU=CDFA47C0'
     JIRA_EMAIL = 'dentzbar@gmail.com'
+    PROJECT_CONNECTION_STRING = 'eastus2.api.azureml.ms;8b46674a-af45-4cd7-a378-82a11c820aa0;rg-dentzbar-7025_ai;dentzbar-9550'
